a54d066e84a80db712c3b002ecb4c37884e5fbd1
Alex Lushiku <alex.lushiku@outlook.com>
2018-12-03 01:03:10 +0100

    fixed 'Uncaught TypeError'

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = require('./main.js');
 const express = require('express');
 const expApp = express();
 const cron = require('node-cron');
-const accounts = require('./accounts');
+const accounts = require('./accounts.js');
 
 expApp.set('port', process.env.PORT || 8080);
 expApp.use(express.static('webpage'));
@@ -11,7 +11,7 @@ expApp.listen(expApp.get('port'));
 var randInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 // *minute, *hour, *day of month, *month, *day of week
-for (var i = 0;i < accounts.length;i++) {
+for (let i = 0;i < accounts.length;i++) {
   cron.schedule(`${randInt(0, 59)} ${randInt(0, 12)},${randInt(13, 23)} * * *`, () => {
     app.run(accounts[i]);
   });
